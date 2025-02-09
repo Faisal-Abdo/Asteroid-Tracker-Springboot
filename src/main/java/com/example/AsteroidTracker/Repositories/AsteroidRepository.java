c2e95ac463c743491caf3e07312d053ee8cc430e
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2025-02-09 12:24:39 +0400

    Added custom queries for the repository

--- src/main/java/com/example/AsteroidTracker/Repositories/AsteroidRepository.java
+++ src/main/java/com/example/AsteroidTracker/Repositories/AsteroidRepository.java
@@ -2,6 +2,7 @@ package com.example.AsteroidTracker.Repositories;
 
 import com.example.AsteroidTracker.Models.Asteroid;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,21 +13,28 @@ import java.util.List;
 @Repository
 public interface AsteroidRepository extends JpaRepository<Asteroid, Integer> {
 
+    @Query("select a from Asteroids a where a.id=:id")
     Asteroid getAsteroidById(@Param(value = "id") Integer id);
 
+    @Query("select a from Asteroids a where a.startDate=CURRENT_DATE and a.endDate=CURRENT_DATE")
     List<Asteroid> getAsteroidsByTodayDate();
 
+    @Query("select a from Asteroids a where a.startDate=:startDate and a.endDate=:endDate")
     List<Asteroid> getAsteroidsWithinRange(@Param(value = "startDate") LocalDate startDate,
                                            @Param(value = "endDate") LocalDate endDate);
 
+    @Query("select a from Asteroids a where a.size > :size")
     List<Asteroid> getAsteroidLargerThan(@Param(value = "size") Double size);
 
-    List<Asteroid> getPotentialHazardousAsteroids(@Param(value = "riskOfCollision")
-                                                  Boolean riskOfCollision);
+    @Query("select a from Asteroids a where a.riskOfCollision = true")
+    List<Asteroid> getPotentialHazardousAsteroids();
 
-    List<Asteroid> getClosestAsteroids(@Param(value = "distance") Long distance);
+    @Query(value = "select a from Asteroids a order by a.distance ASC limit=:count ", nativeQuery = true)
+    List<Asteroid> getClosestAsteroids(@Param(value = "count") Integer count);
 
-    List<Asteroid> getFastestAsteroids(@Param(value = "speed") Double speed);
+    @Query(value = "select a from Asteroids a order by a.speed DESC limit=:count", nativeQuery = true)
+    List<Asteroid> getFastestAsteroids(@Param(value = "count") Integer count);
 
-    List<Asteroid> getLargestAsteroids(@Param(value = "size") Double size);
+    /*@Query(value = "select a from Asteroids a order by a.size DESC limit=:count", nativeQuery = true)
+    List<Asteroid> getLargestAsteroids(@Param(value = "count") Integer count);*/
 }

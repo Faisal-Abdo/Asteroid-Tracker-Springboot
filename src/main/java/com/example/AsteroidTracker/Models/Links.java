341b721c29ebce088c9574bad702b5a8de166413
Faisal-Abdo <faisalalbusaidy2015@gmail.com>
2025-02-10 17:59:54 +0400

    Create Links.java

---
+++ src/main/java/com/example/AsteroidTracker/Models/Links.java
@@ -0,0 +1,39 @@
+package com.example.AsteroidTracker.Models;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class Links {
+
+    @JsonProperty("next")
+    private String next;
+
+    @JsonProperty("previous")
+    private String previous;
+
+    @JsonProperty("self")
+    private String self;
+
+    public String getNext() {
+        return next;
+    }
+
+    public void setNext(String next) {
+        this.next = next;
+    }
+
+    public String getPrevious() {
+        return previous;
+    }
+
+    public void setPrevious(String previous) {
+        this.previous = previous;
+    }
+
+    public String getSelf() {
+        return self;
+    }
+
+    public void setSelf(String self) {
+        this.self = self;
+    }
+}
